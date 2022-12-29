16a6ee7e5a7c8854c6b3e110212528b0e85cef47
MorrisLCY <morris.cy0910@gmail.com>
2022-12-29 11:40:32 +0800

    Ch5 click events

--- src/Content.js
+++ src/Content.js
@@ -5,11 +5,26 @@ const Content = () => {
         return name[int];
     }
 
+    const handleClick = () =>{
+        console.log("YOU CLICKED !")
+    }
+
+    const handleNameClick = (name) =>{
+        console.log(`${name} was clicked`)
+    }
+
+    const handleEventClick = (e) =>{
+        console.log(e.target.innerText)
+    }
+
     return(
         <main>
             <p>
                 Hello {handleNameChange()} !!
             </p>
+            <button onClick={handleClick}>CLICKED</button>
+            <button onClick={() => handleNameClick('Morris')}>NAME CLICKED</button>
+            <button onClick={(e) => handleEventClick(e)}>Event</button>
         </main>
     )
 }
