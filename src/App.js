3d311e1bdce39f9c8cea1e81468a4d74c8150209
MorrisLCY <morris.cy0910@gmail.com>
2022-12-29 10:08:18 +0800

    Finished Ch2 App&JSX

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleName = () =>{
+    const names = ['Morris', 'Sylvia', 'Teresa'];
+    const int = Math.floor(Math.random()*3);
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see changes.
+          Hello {handleName()} !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>JSX is a javascript & React Template of html</p>
+        <p>we can't do object in object </p>
       </header>
     </div>
   );
